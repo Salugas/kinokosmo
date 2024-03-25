103220cdf69c804421b966532837b26e74105944
Daniil Salygin <danya.salygin@mail.ru>
2024-03-25 13:56:03 +0300

    init

--- src/models/Top10MoviesCarusel/components/Top10MoviesCarusel/Top10MoviesCarusel.tsx
+++ src/models/Top10MoviesCarusel/components/Top10MoviesCarusel/Top10MoviesCarusel.tsx
@@ -4,7 +4,7 @@ import { Top10MoviesAPI } from 'models/Top10MoviesCarusel/API/Top10MoviesAPI'
 import { MoviesList } from '../Movies/MoviesList'
 
 export const Top10MoviesCarusel = ({}) => {
-	const { data } = Top10MoviesAPI.useFetchTop10MoviesQuery('')
+	const { data } = Top10MoviesAPI.useFetchTop10MoviesQuery({})
 
 	const { docs: movies }: any = { ...data }
 
