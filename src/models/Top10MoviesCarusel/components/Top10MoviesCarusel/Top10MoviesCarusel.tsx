cbadb468d93afdaf49ca1b5735c4ba06113425a0
Daniil Salygin <danya.salygin@mail.ru>
2024-03-25 14:01:53 +0300

    init

--- src/models/Top10MoviesCarusel/components/Top10MoviesCarusel/Top10MoviesCarusel.tsx
+++ src/models/Top10MoviesCarusel/components/Top10MoviesCarusel/Top10MoviesCarusel.tsx
@@ -3,7 +3,7 @@ import cl from './Top10MoviesCarusel.module.scss'
 import { Top10MoviesAPI } from 'models/Top10MoviesCarusel/API/Top10MoviesAPI'
 import { MoviesList } from '../Movies/MoviesList'
 
-export const Top10MoviesCarusel = ({}) => {
+export const Top10MoviesCarusel = () => {
 	const { data } = Top10MoviesAPI.useFetchTop10MoviesQuery('')
 
 	const { docs: movies }: any = { ...data }
