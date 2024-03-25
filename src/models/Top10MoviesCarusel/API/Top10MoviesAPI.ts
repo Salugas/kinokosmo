6ff9cfef9a030ed131355f0e27b5827cb904821b
Daniil Salygin <danya.salygin@mail.ru>
2024-03-25 13:49:47 +0300

    change top10

--- src/models/Top10MoviesCarusel/API/Top10MoviesAPI.ts
+++ src/models/Top10MoviesCarusel/API/Top10MoviesAPI.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { API_KEY, API_URL } from '../../../contants/api'
-import { IMovies, IMoviesBackdrop } from 'types/IMovies'
+import { IMovies } from 'types/IMovies'
 
 export const Top10MoviesAPI = createApi({
 	reducerPath: 'Top10Movies',
